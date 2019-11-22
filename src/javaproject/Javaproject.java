fcd638e670904d51f63d510dc46d2668e265a8b3
tibonuyts <tibonuytsbackup@gmail.com>
2019-11-22 22:14:25 +0100

    connector

--- src/javaproject/Javaproject.java
+++ src/javaproject/Javaproject.java
@@ -15,7 +15,7 @@ public class Javaproject {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("tibo");
+        System.out.println("tjkhkjibo");
         
     }
     
