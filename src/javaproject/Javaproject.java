4c8e25a8e849d26cad5e051b093fa283d062a91c
tibonuyts <tibonuytsbackup@gmail.com>
2019-11-22 22:06:05 +0100

    test

--- src/javaproject/Javaproject.java
+++ src/javaproject/Javaproject.java
@@ -15,7 +15,7 @@ public class Javaproject {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("");
+        System.out.println("tibo");
         
     }
     
