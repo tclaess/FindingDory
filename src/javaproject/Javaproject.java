92deef6e6ae927e02d423110999255afccae67a5
Mathis99 <58071984+Mathis99@users.noreply.github.com>
2019-11-26 13:24:07 +0100

    system test

--- src/javaproject/Javaproject.java
+++ src/javaproject/Javaproject.java
@@ -16,7 +16,7 @@ public class Javaproject {
      */
     public static void main(String[] args) {
         System.out.println("tjkhkjibosuckt");
-        
+        System.out.println("sdfsfsdsdfsdff");
     }
     
 }
