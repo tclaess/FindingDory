7042cf80b43679b9034ca0dc73b8e2c9d07537d4
aroenh <58072325+aroenh@users.noreply.github.com>
2019-11-28 16:17:51 +0100

    Starwindowcontroller

--- src/project/gui/StartWindowController.java
+++ src/project/gui/StartWindowController.java
@@ -5,17 +5,32 @@
  */
 package project.gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
 
 /**
  * FXML Controller class
  *
- * @author mathi
+ * @author aroen
  */
 public class StartWindowController implements Initializable {
 
+    @FXML
+    private Button BoekReisBtn;
+    @FXML
+    private Button ZoekBoekingBtn;
+    @FXML
+    private Button KlantenbestandBtn;
+    private AnchorPane dataPan;
     /**
      * Initializes the controller class.
      */
@@ -23,5 +38,43 @@ public class StartWindowController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
-    
+
+    @FXML
+    private void BoekReis(ActionEvent event) {
+    try {
+      
+      
+      AnchorPane pane = (AnchorPane) FXMLLoader.load(getClass().getResource("/tricksproject/gui/OptiesReis.fxml"));
+      dataPan.getChildren().setAll(pane);
+      
+      
+    } catch (IOException ex) {
+      Logger.getLogger(StartWindowController.class.getName()).log(Level.SEVERE, null, ex);
+    }
+  }
+
+  
+    @FXML
+    private void ZoekBoeking(ActionEvent event) {
+    try {
+      AnchorPane pane = (AnchorPane) FXMLLoader.load(getClass().getResource("/tricksproject/gui/OptiesBoeking.fxml"));
+      dataPan.getChildren().setAll(pane);
+    } catch (IOException ex) {
+      Logger.getLogger(StartWindowController.class.getName()).log(Level.SEVERE, null, ex);
+    }
+  }
+    @FXML
+   private void OptiesKlanten(ActionEvent event) {
+    try {
+      AnchorPane pane = (AnchorPane) FXMLLoader.load(getClass().getResource("/tricksproject/gui/OptiesKlanten.fxml"));
+      dataPan.getChildren().setAll(pane);
+    } catch (IOException ex) {
+      Logger.getLogger(StartWindowController.class.getName()).log(Level.SEVERE, null, ex);
+    }
+  }
+
+  
+  
 }
+
+
