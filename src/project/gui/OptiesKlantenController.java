36f922dad8ed8f9f2b99a2695db0a2e489fe5b46
mathi <mathi@192.168.0.100>
2019-12-06 03:22:05 +0100

    test GUI

---
+++ src/project/gui/OptiesKlantenController.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project.gui;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author mathi
+ */
+public class OptiesKlantenController implements Initializable {
+
+    @FXML
+    private Button klantToevoegenBtn;
+    @FXML
+    private Button klantInfoBtn;
+    @FXML
+    private Button klantUpdateBtn;
+    @FXML
+    private Button terugBtn;
+    @FXML
+    private Button klantRapportBtn;
+    
+    @FXML
+    public void klantToevoegenBtnPushed(ActionEvent event) throws IOException
+    {
+        Parent klantToevoegenParent = FXMLLoader.load(getClass().getResource("/project/gui/KlantToevoegen.fxml"));
+        Scene klantToevoegenScene = new Scene(klantToevoegenParent);
+        
+        //This line gets the Stage information
+        Stage window = FXMLLoader.load(getClass().getResource("/project/gui/KlantToevoegen.fxml"));
+        
+        window.setScene(klantToevoegenScene);
+        window.show();
+    }
+    
+    public void terugBtnPushed (ActionEvent event) throws IOException
+    {
+        Parent startWindowParent = FXMLLoader.load(getClass().getResource("/project/gui/StartWindow.fxml"));
+        Scene startWindowScene = new Scene(startWindowParent);
+        
+        Stage window = FXMLLoader.load(getClass().getResource("/project/gui/StartWindow.fxml")); 
+        
+        window.setScene(startWindowScene);
+        window.show();
+    }
+    
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
--- src/project/gui/StartWindowController.java
+++ src/project/gui/StartWindowController.java
@@ -5,9 +5,17 @@
  */
 package project.gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -15,6 +23,48 @@ import javafx.fxml.Initializable;
  * @author mathi
  */
 public class StartWindowController implements Initializable {
+
+    @FXML
+    private Button boekReisBtn;
+    @FXML
+    private Button zoekBoekingBtn;
+    @FXML
+    private Button klantenbestandBtn;
+    
+    public void boekReisBtnPushed (ActionEvent event) throws IOException
+    {
+        Parent boekReisParent = FXMLLoader.load(getClass().getResource("/project/gui/OptiesReis.fxml"));
+        Scene boekReisScene = new Scene(boekReisParent);
+        
+        Stage window = FXMLLoader.load(getClass().getResource("/project/gui/OptiesReis.fxml")); 
+        
+        window.setScene(boekReisScene);
+        window.show();
+    }
+    
+    public void zoekBoekingBtnPushed (ActionEvent event) throws IOException
+    {
+        Parent zoekBoekingParent = FXMLLoader.load(getClass().getResource("/project/gui/OptiesBoeking.fxml"));
+        Scene zoekBoekingScene = new Scene(zoekBoekingParent);
+        
+        Stage window = FXMLLoader.load(getClass().getResource("/project/gui/OptiesBoeking.fxml")); 
+        
+        window.setScene(zoekBoekingScene);
+        window.show();
+    }
+    
+    public void klantenbestandBtnPushed (ActionEvent event) throws IOException
+    {
+        Parent klantenbestandParent = FXMLLoader.load(getClass().getResource("/project/gui/OptiesKlanten.fxml"));
+        Scene klantenbestandScene = new Scene(klantenbestandParent);
+        
+        Stage window = FXMLLoader.load(getClass().getResource("/project/gui/OptiesKlanten.fxml")); 
+        
+        window.setScene(klantenbestandScene);
+        window.show();
+    }
+    
+    
 
     /**
      * Initializes the controller class.
