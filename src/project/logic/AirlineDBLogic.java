10b5c0b506cedc9d19e8ba9c0963b18975789fb4
aroenh <58072325+aroenh@users.noreply.github.com>
2019-12-09 21:45:18 +0100

    Starwindowcontroller

---
+++ src/project/logic/AirlineDBLogic.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project.logic;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author aroen
+ */
+public class AirlineDBLogic {
+    private static ArrayList<Airline> airlines;
+    private static AirlineDBLogic airlineDBLogic = new AirlineDBLogic();
+}
+
+
